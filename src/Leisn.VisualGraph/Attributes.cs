2675a708a5d028b61abd4110251467222cdf4267
leisn <cando.i@outlook.com>
2025-04-07 15:25:07 +0800

    add Attributes

---
+++ src/Leisn.VisualGraph/Attributes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Leisn.VisualGraph;
+
+[AttributeUsage(AttributeTargets.All, Inherited = true)]
+public class IgnoreAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Property, Inherited = true)]
+public class InAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Property, Inherited = true)]
+public class OutAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class InvokeAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class InitAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class FinalAttribute : Attribute { }
